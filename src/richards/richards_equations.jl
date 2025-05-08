a5a4acdc9e8b5c3621697a7c72225d00000c702c
Huite <huite.bootsma@gmail.com>
2025-05-08 10:11:55 +0200

    Clean richards a little

--- src/richards/richards_equations.jl
+++ src/richards/richards_equations.jl
@@ -113,37 +113,26 @@ end
 # Full column
 
 function waterbalance!(state::RichardsState, parameters::RichardsParameters)
-    @. state.k = conductivity(state.ψ, parameters.constitutive)
-    @. state.dk = dconductivity(state.ψ, parameters.constitutive)
+    (; ψ, ∇q, θ, C, k, k_inter, Δψ, forcing) = state
+    (; constitutive, Δz, Δz⁻¹, forcing, bottomboundary, topboundary, n) = parameters
+    @. k = conductivity(ψ, constitutive)
+    @. C = specific_moisture_capacity(ψ, constitutive)
+    @. θ = moisture_content(ψ, constitutive)
 
-    k_lower = @view(state.k[1:end-1])
-    Δz_lower = @view(parameters.Δz[1:end-1])
-    k_upper = @view(state.k[2:end])
-    Δz_upper = @view(parameters.Δz[2:end])
-    ψ_upper = @view state.ψ[2:end]
-    ψ_lower = @view state.ψ[1:end-1]
-
-    @. state.Δψ = ψ_upper - ψ_lower
-    @. state.k_inter = (k_lower * Δz_lower + k_upper * Δz_upper) / (Δz_lower + Δz_upper)
-    @. state.kΔz⁻¹ = state.k_inter / (0.5 * Δz_lower + 0.5 * Δz_upper)
-    @. state.kΔψΔz⁻¹ = state.Δψ * state.kΔz⁻¹
-    @. state.ΔψΔz⁻¹ = state.Δψ / (0.5 * Δz_lower + 0.5 * Δz_upper) + 1.0
-
-    # Moisture capacity
-    @. state.C = specific_moisture_capacity(state.ψ, parameters.constitutive)
-
-    # Moisture content
-    @. state.θ = moisture_content(state.ψ, parameters.constitutive)
+    lower = (1:n-1)
+    upper = (2:n)
+    @. Δψ = ψ[upper] - ψ[lower]
+    @. k_inter = (k[lower] * Δz[lower] + k[upper] * Δz[upper]) / (Δz[lower] + Δz[upper])
 
     # Internodal flows
-    @. state.∇q = 0.0
-    @views @. state.∇q[2:end] -= (state.kΔψΔz⁻¹ + state.k_inter)  # i-1 terms
-    @views @. state.∇q[1:end-1] += (state.kΔψΔz⁻¹ + state.k_inter)  # i+1 terms
+    @. ∇q = 0.0
+    @views @. ∇q[2:end] -= k_inter * (Δψ * Δz⁻¹ + 1)  # i-1 terms
+    @views @. ∇q[1:end-1] += k_inter * (Δψ * Δz⁻¹ + 1)  # i+1 terms
 
     # Boundary conditions
-    state.∇q[1] += bottomflux(state, parameters, parameters.bottomboundary)
-    state.∇q[end] += topflux(state, parameters, parameters.topboundary)
-    state.∇q[end] += topflux(state, parameters, parameters.forcing)
+    ∇q[1] += bottomflux(state, parameters, bottomboundary)
+    ∇q[end] += topflux(state, parameters, topboundary)
+    ∇q[end] += topflux(state, parameters, forcing)
     return
 end
 
@@ -168,34 +157,30 @@ end
     Use Δt = ∞ for steady-state simulations.
 """
 function jacobian!(J, state::RichardsState, parameters::RichardsParameters, Δt)
-    # dFᵢ/dψᵢ₋₁ = (kΔz⁻¹)|ᵢ₋₁ - dk/dψ|ᵢ₋₁ * (ΔψΔz⁻¹ + 1) * Δzᵢ₋₁ / (Δzᵢ₋₁ + Δzᵢ)
-    # dFᵢ/dψᵢ = -CΔz/Δt 
-    #          - (kΔz⁻¹)|ᵢ₋₁ + dk/dψ|ᵢ * (ΔψΔz⁻¹ + 1)|ᵢ₋₁ * Δzᵢ / (Δzᵢ₋₁ + Δzᵢ)
-    #          - (kΔz⁻¹)|ᵢ₊₁ - dk/dψ|ᵢ * (ΔψΔz⁻¹ + 1)|ᵢ₊₁ * Δzᵢ / (Δzᵢ₊₁ + Δzᵢ)
-    # dFᵢ/dψᵢ₊₁ = (kΔz⁻¹)|ᵢ₊₁ + dk/dψ|ᵢ₊₁ * (ΔψΔz⁻¹ + 1) * Δzᵢ₊₁ / (Δzᵢ₊₁ + Δzᵢ)
+    (; ψ, C, k_inter, Δψ, dk, forcing) = state
+    (; constitutive, Δz, Δz⁻¹, forcing, bottomboundary, topboundary, n) = parameters
+    @. dk = dconductivity(ψ, constitutive)
 
     dFᵢdψᵢ = J.d  # derivatives of F₁, ... Fₙ with respect to ψ₁, ... ψₙ
     dFᵢ₊₁dψᵢ = J.dl  # derivatives of F₂, ... Fₙ with respect to ψ₁, ... ψₙ₋₁
     dFᵢ₋₁dψᵢ = J.du  # derivatives of F₁, ... Fₙ₋₁ with respect to ψ₂, ... ψₙ
 
-    # TODO: Probably swap these variable names
-    dkᵢ₋₁ = @view(state.dk[1:end-1])
-    dkᵢ₊₁ = @view(state.dk[2:end])
-    Δzᵢ₋₁ = @view(parameters.Δz[1:end-1])
-    Δzᵢ₊₁ = @view(parameters.Δz[2:end])
-
+    lower = (1:n-1)
+    upper = (2:n)
     # First compute the off-diagonal terms
-    @. dFᵢ₊₁dψᵢ = state.kΔz⁻¹ - dkᵢ₋₁ * state.ΔψΔz⁻¹ * Δzᵢ₋₁ / (Δzᵢ₋₁ + Δzᵢ₊₁)
-    @. dFᵢ₋₁dψᵢ = state.kΔz⁻¹ + dkᵢ₊₁ * state.ΔψΔz⁻¹ * Δzᵢ₊₁ / (Δzᵢ₊₁ + Δzᵢ₋₁)
+    @. dFᵢ₊₁dψᵢ =
+        (k_inter * Δz⁻¹) - dk[lower] * (Δψ * Δz⁻¹) * Δz[lower] / (Δz[lower] + Δz[upper])
+    @. dFᵢ₋₁dψᵢ =
+        (k_inter * Δz⁻¹) + dk[lower] * (Δψ * Δz⁻¹) * Δz[upper] / (Δz[upper] + Δz[lower])
 
     # Then compute the diagonal term
-    @. dFᵢdψᵢ = -(state.C * parameters.Δz) / Δt
+    @. dFᵢdψᵢ = -(C * Δz) / Δt
     @views @. dFᵢdψᵢ[1:end-1] += -dFᵢ₊₁dψᵢ
     @views @. dFᵢdψᵢ[2:end] += -dFᵢ₋₁dψᵢ
 
-    J.d[1] += bottomboundary_jacobian!(state, parameters, parameters.bottomboundary)
-    J.d[end] += topboundary_jacobian!(state, parameters, parameters.topboundary)
-    J.d[end] += topboundary_jacobian!(state, parameters, parameters.forcing)
+    J.d[1] += bottomboundary_jacobian!(state, parameters, bottomboundary)
+    J.d[end] += topboundary_jacobian!(state, parameters, topboundary)
+    J.d[end] += topboundary_jacobian!(state, parameters, forcing)
     return
 end
 
--- src/richards/richards_parameters.jl
+++ src/richards/richards_parameters.jl
@@ -1,7 +1,23 @@
 struct RichardsParameters{C,T,B} <: Parameters
     constitutive::Vector{C}
     Δz::Vector{Float}
+    Δz⁻¹::Vector{Float}
     forcing::MeteorologicalForcing
     bottomboundary::B
     topboundary::T
+    n::Int
+end
+
+function RichardsParameters(constitutive, Δz, forcing, bottomboundary, topboundary)
+    Δz⁻¹ = @views 2.0 ./ (Δz[1:end-1] .+ Δz[2:end])
+    n = length(constitutive)
+    return RichardsParameters(
+        constitutive,
+        Δz,
+        Δz⁻¹,
+        forcing,
+        bottomboundary,
+        topboundary,
+        n,
+    )
 end
